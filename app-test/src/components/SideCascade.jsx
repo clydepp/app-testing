daeddf8543a02f3e6128e28e5ebd7ecc7abe30d0
Clyde Pangilinan <pangiclyde@gmail.com>
2025-06-19 10:19:56 +0100

    Added some more UI functionality - need to change icons and tutorial.

--- app-test/src/components/SideCascade.jsx
+++ app-test/src/components/SideCascade.jsx
@@ -4,11 +4,9 @@ import Button from './Button.jsx'
 export default function SideCascade (props) {
     const [isSide, setIsSide] = createSignal(false);
 
-    const handleClick = () => {
+    const handleMainButtonClick = () => {
         if (props.showNumbers && props.onMinusOne) {
             props.onMinusOne();
-        } else {
-            setIsSide(!isSide());
         }
     };
 
@@ -19,9 +17,21 @@ export default function SideCascade (props) {
         }
     };
 
+    const handleMouseEnter = () => {
+        setIsSide(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsSide(false);
+    };
+
     return (
-        <div class="flex flex-row gap-2">  {/* ← Change to flexbox */}
-            <Button onClick={handleClick}>
+        <div 
+            class="flex flex-row gap-2"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            <Button onClick={handleMainButtonClick}>
                 { props.showNumbers ? "+3" : (
                     <svg width="20" height="20" data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3 8.25V18a2.25 2.25 0 0 0 2.25 2.25h13.5A2.25 2.25 0 0 0 21 18V8.25m-18 0V6a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 6v2.25m-18 0h18M5.25 6h.008v.008H5.25V6ZM7.5 6h.008v.008H7.5V6Zm2.25 0h.008v.008H9.75V6Z" stroke-linecap="round" stroke-linejoin="round"></path>
